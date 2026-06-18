d2c9be4483c2c3bc96b00d9099eceb98b00dea15
Sören <soeren.todenhagen@htwg-konstanz.de>
2026-06-18 19:57:48 +0200

    chore: E-Mail-Infrastruktur vollständig entfernen
    - spring-boot-starter-mail aus pom.xml entfernt
    - SMTP/Mailjet-Konfiguration aus application.properties entfernt
    - EmailService, EmailLog, EmailType, EmailLogRepository gelöscht
    - EmailLogController, ContactController, SystemSettingController gelöscht
    - SystemSetting, SystemSettingRepository gelöscht

--- src/main/java/de/htwg/in/nexcare/backend/repository/SystemSettingRepository.java
+++
@@ -1,6 +0,0 @@
-package de.htwg.in.nexcare.backend.repository;
-
-import de.htwg.in.nexcare.backend.model.SystemSetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SystemSettingRepository extends JpaRepository<SystemSetting, String> {}
